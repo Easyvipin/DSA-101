b4b70fc54561c17c5b24bd56e42798597a3bbf49
vipin <vipinchandra555@gmail.com>
2022-07-04 07:56:47 +0530

    added averagePair solution

---
+++ Problem Solving Patterns/averagePair.js
@@ -0,0 +1,28 @@
+// waf to check a if avg of pair is true as of given avg
+
+/* 
+examples 
+averagePair([1,2,3],2.5) // true
+averagePair([],4) // false
+
+*/
+
+function averagePair(arry, num) {
+  if (arry.length === 0) {
+    return false;
+  }
+  let start = 0;
+  let end = arry.length - 1;
+
+  while (start < end) {
+    let avg = (arry[start] + arry[end]) / 2;
+    console.log(arry[start], arry[end]);
+    if (avg === num) return true;
+    else if (avg < num) start++;
+    else end--;
+    console.log(end);
+  }
+  return false;
+}
+
+averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
